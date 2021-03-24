ceb7852bcd481106ba27b2e1b8ce0ef596e01c62
Raghvendra Singh <32678356+Raghvendrars@users.noreply.github.com>
2021-03-24 14:21:44 +0530

    Create register.php

---
+++ 14/register.php
@@ -0,0 +1,28 @@
+<?php
+
+    $l="localhost";
+    $r="root";
+    $p="";
+    $db="studreg";
+
+    $con=mysqli_connect($l,$r,$p,$db);
+
+    if ($con) {
+        echo "Suc";
+    }
+
+    $Cust_no=$_POST['cust_num'];
+    $Cust_name=$_POST['cust_name'];
+    $Item_purchased=$_POST['item_pur'];
+    $Mob_no=$_POST['mbl_num'];
+
+    $sql="INSERT INTO customer (Cust_no,Cust_name,Item_purchased,Mob_no) values ('$Cust_no','$Cust_name','$Item_purchased','$Mob_no')";
+
+    if ($con->query($sql) === TRUE) {
+        header('Location: index.php');
+      } else {
+        echo "Error: " . $sql . "<br>" . $con->error;
+      }  
+      $con->close();
+    
+?>
